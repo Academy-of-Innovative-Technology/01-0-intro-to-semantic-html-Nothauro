e1291d95c364c4e5984c925b4fa295541a4501e7
Thad <Thadphillp35@gmail.com>
2025-11-19 12:24:52 -0500

    Done with everything

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let main = document.querySelector("main")
 
-let posts = {
+let posts = [{
     image: "https://www.eggoz.com/cdn/shop/articles/WhatsApp_Image_2024-09-07_at_5.05.44_PM.jpg?v=1725713796&width=1100",
     title: "You big stop eating all that fast food",
     post: "Fast Food"
@@ -14,6 +14,12 @@ let posts = {
     image: "https://spoonuniversity.com/cdn-cgi/image/width=1200,fit=cover,format=auto/https://s3.amazonaws.com/secretsaucefiles/photos/images/000/115/554/large/Gabrielle-Taylor.jpg",
     title: "bad food you should not eat",
     post: "shut up dirty hoe"
-};
-posts.forEach()
-    main.insertAdjacentHTML
+}
+];
+posts.forEach(function(post){
+     main.insertAdjacentHTML('beforeend',`
+      <img src = ${post.image}>
+     ${post.title}
+     ${post.post}
+     `);
+});
